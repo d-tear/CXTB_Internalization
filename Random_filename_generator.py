f04850fa446a46e03654e29fd726246a8242397d
David Tyrpak <davidtyrpak@Davids-MacBook.attlocal.net>
2018-07-10 16:10:39 -0700

    renamed the variables which had "tiff" in their name to something clearer and more general.

--- Random_filename_generator.py
+++ Random_filename_generator.py
@@ -21,42 +21,40 @@ def listdir_nohidden(path): ## this function returns all the files in a director
 
 input_directory = "/Users/davidtyrpak/Desktop/python_playground"
 output_directory = "/Users/davidtyrpak/Desktop/python_playground/output" ##Make sure this directory is empty!
-
-print(os.listdir(output_directory))
+extension = ".txt"
 
 if len(listdir_nohidden(output_directory)) != 0:
     raise RuntimeError("Warning! Your output_directory is not empty! Please choose an empty directory.")
 
 
 i = 0
-tiff_list = []
+file_list = []
     
 for f in os.listdir(input_directory):
-    if f.endswith(".txt"):
+    if f.endswith(extension):
         i+=1
-        tiff_list.append(f)
+        file_list.append(f)
         print(i)
     
         if i==0:
-            print("There are no tiff files in this folder!")
+            print("There are no files in this folder!")
         else:
             pass
 
 
-number_of_tiff_files = len(tiff_list) 
-print(tiff_list)
-print(number_of_tiff_files)
+number_of_files = len(file_list) 
 
-output_tiff_list = [] ## This for loop copies the tiff files from the input directory into the output directory
-for tiff_file in tiff_list:
-    full_tiff_name = os.path.join(input_directory, tiff_file)
-    copyfile(full_tiff_name, os.path.join(output_directory, tiff_file)) ## copy file to output directory
 
-tiff_output_list = [] ## I should make a test case here. I should test that len(tiff_output_list) == len(tiff_list)
+output_file_list = [] ## This for loop copies the files from the input directory into the output directory
+for file in file_list:
+    full_file_name = os.path.join(input_directory, file)
+    copyfile(full_file_name, os.path.join(output_directory, file)) ## copy file to output directory
+
+file_output_list = [] ## I should make a test case here.
 
 for f in os.listdir(output_directory):
-    if f.endswith(".txt"):
-        tiff_output_list.append(f)
+    if f.endswith(extension):
+        file_output_list.append(f)
 
 print(tiff_output_list)
 print(tiff_output_list)
@@ -65,25 +63,24 @@ print(tiff_output_list)
 
 random_list = []
 
-while len(random_list) < len(tiff_list):
+while len(random_list) < len(file_list):
     n = random.randint(1, 100) ##if you need to randomize more than 100 files, select a higher number
-    assert(len(tiff_list) <= 100)
+    assert(len(file_list) <= 100)
     if n not in random_list:
         random_list.append(n)
     else:
         pass
 
-print(random_list)
-print(len(random_list))
+
 
 i = 0
 number_to_filename_dict = {} ## create empty dictionary. This will store original filenames and their corresponding random number.
 
 
-for tiff_file in tiff_output_list:
-    print(tiff_file)
-    os.rename(os.path.join(output_directory, tiff_file), os.path.join(output_directory, str(random_list[i])+ ".txt"))
-    number_to_filename_dict[tiff_file] = random_list[i]
+for file in file_output_list:
+    print(file)
+    os.rename(os.path.join(output_directory, file), os.path.join(output_directory, str(random_list[i])+ extension))
+    number_to_filename_dict[file] = random_list[i]
     i = i + 1
     print(i)
 
